1d36d7fa774ec4e525b822652137aeda41625a61
MALLESH C N <84223327+rohanworld@users.noreply.github.com>
2023-07-23 02:50:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+$(document).ready(function () {
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times");
+    $("header").toggleClass("toggle");
+  });
+
+  $(window).on("scroll load", function () {
+    $("#menu").removeClass("fa-times");
+    $("header").removeClass("toggle");
+
+    if ($(window).scrollTop() > 0) {
+      $(".top").show();
+    } else {
+      $(".top").hide();
+    }
+  });
+
+  $('a[href*="#"]').on("click", function (e) {
+    e.preventDefault();
+
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
+      500,
+      "linear"
+    );
+  });
+});
+
+
+// document.getElementById("viewProject").addEventListener("click", function () {
+//   window.open("https://malleshcn.netlify.app/projects/index.html", "_blank");
+// });
+
+
+(function ($) {
+  "use strict";
+
+  $(document).ready(function () {
+    "use strict";
+
+    var progressPath = document.querySelector(".progress-wrap path");
+    var pathLength = progressPath.getTotalLength();
+    progressPath.style.transition = progressPath.style.WebkitTransition =
+      "none";
+    progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+    progressPath.style.strokeDashoffset = pathLength;
+    progressPath.getBoundingClientRect();
+    progressPath.style.transition = progressPath.style.WebkitTransition =
+      "stroke-dashoffset 10ms linear";
+    var updateProgress = function () {
+      var scroll = $(window).scrollTop();
+      var height = $(document).height() - $(window).height();
+      var progress = pathLength - (scroll * pathLength) / height;
+      progressPath.style.strokeDashoffset = progress;
+    };
+    updateProgress();
+    $(window).scroll(updateProgress);
+    var offset = 50;
+    var duration = 550;
+    jQuery(window).on("scroll", function () {
+      if (jQuery(this).scrollTop() > offset) {
+        jQuery(".progress-wrap").addClass("active-progress");
+      } else {
+        jQuery(".progress-wrap").removeClass("active-progress");
+      }
+    });
+    jQuery(".progress-wrap").on("click", function (event) {
+      event.preventDefault();
+      jQuery("html, body").animate({ scrollTop: 0 }, duration);
+      return false;
+    });
+  });
+})(jQuery);
+
+$(`.circlechart`).circlechart();
+
+function validateForm() {
+  var nameInput = document.getElementById("name");
+  var emailInput = document.getElementById("email");
+
+  var nameRegex = /^[a-zA-Z\s]+$/;
+  var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (!nameInput.value.match(nameRegex)) {
+    nameInput.focus();
+    return false;
+  }
+  if (!emailInput.value.match(emailRegex)) {
+    emailInput.focus();
+    return false;
+  }
+  return true;
+}
